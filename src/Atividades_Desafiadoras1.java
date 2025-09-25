6ab3d6e3e570fd5c67a5cfe9b4bbcd4c4d018d9b
Moacir samoel anhaia <samoel.anhaia@outlook.com>
2025-09-25 11:28:52 -0300

    Feito uma matriz 5x5 mostrando apenas a diagonal com 1 e o restante com 0.

---
+++ src/Atividades_Desafiadoras1.java
@@ -0,0 +1,30 @@
+public class Atividades_Desafiadoras1 {
+    public static void main(String[] args) {
+        int[][] matriz = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------");
+
+        for (int i = 0; i < 5; i++) {
+            matriz[i][i] = 1;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
